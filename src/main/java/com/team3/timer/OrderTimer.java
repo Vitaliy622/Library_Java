c2e700fe750d9b79fcb7830870ceb39c9e0ab585
Vitaliy622 <vitalikyarick@gmail.com>
2021-11-16 14:32:20 +0200

    add timer that check if is not break deadlines

---
+++ src/main/java/com/team3/timer/OrderTimer.java
@@ -0,0 +1,64 @@
+package com.team3.timer;
+
+import com.team3.entity.Order;
+import com.team3.entity.OrderStatus;
+import com.team3.service.OrderService;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Component
+@EnableScheduling
+public class OrderTimer {
+
+    private final long currentTime = System.currentTimeMillis();
+    private final OrderService orderService;
+
+    public OrderTimer(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @Scheduled(cron = "*/10 * * * * *")
+    public void checkOrders() {
+        checkOrdersIfBooksReturnedInTime();
+        checkIfReservationDate();
+    }
+
+    public void checkOrdersIfBooksReturnedInTime() {
+        for (Order order : orderService.findAllBorrowed()) {
+            long deadline = order.getDeadline().getTime();
+            if (order.getOrderStatus() == OrderStatus.BORROWED && currentTime > deadline) {
+                order.setOrderStatus(OrderStatus.BANNED);
+                orderService.updateOrder(order);
+            }
+        }
+    }
+
+    public long checkIfNotNull(Date date) {
+        if (date != null) {
+            return date.getTime();
+        } else return 0;
+    }
+
+    public void checkIfReservationDate() {
+        for (Order order : orderService.findAllOrders()) {
+            long reservationDeadline = addOneDay(order.getReserveDate()).getTime();
+
+            if (currentTime > reservationDeadline
+                    && order.getOrderStatus() == OrderStatus.RESERVED) {
+                order.setOrderStatus(OrderStatus.CANCELED);
+                orderService.updateOrder(order);
+            }
+        }
+    }
+
+    private Date addOneDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, 2);
+        return new Date(calendar.getTimeInMillis());
+    }
+}
